b77ca1e63cf5940e60e56c2bf20031f0b04469d0
Anna <git@anna.sapek.com>
2015-03-21 17:56:36 -0700

    Score and top common artists computation

--- calculate.py
+++ calculate.py
@@ -8,32 +8,40 @@ def get_score(me, friend):
 	period = '1month'
 	data = [api_call(me, period), api_call(friend, period)]
 	
-	score = 0
-	
 	# dictionaries from artist to rank
 	my_artists = get_artist_dictionary(data[0])
 	friend_artists = get_artist_dictionary(data[1])
 	
+	# score
+	score = 0
+	
+	# n + 1, so min_length - rank ranges from
+	min_length = min(len(my_artists), len(friend_artists)) + 1	
+	
+	# (n + 1)(n) 
+	max = min_length * (min_length - 1)
+	
+	# common artists
+	common_artists = {}
+	top_artists = []
+	
+	# compute a cumulative score for each shared artist
 	for artist, rank in my_artists.iteritems():
 		if artist in friend_artists.iterkeys():
-			print artist, rank, friend_artists[artist]
+			my_score = min_length - int(rank)
+			friend_score = min_length - int(friend_artists[artist])
+			sum_score = my_score + friend_score
+			common_artists[sum_score] = artist
+			score += sum_score
+			print rank, artist, friend_artists[artist], my_score, friend_score, my_score + friend_score
 	
-	'''
-	for data_set in data:
-		# pretty printed data
-		prettyjson = json.dumps(data_set, sort_keys=True, indent=4, separators=(',', ': '))
-
-		# elements
-		root = data_set['topartists']
-		artists = root['artist']
-
-		# artist info
-		for artist in artists:
-			rank = artist['@attr']['rank']
-			name = artist['name']
-			print rank, name
-		#return artists
-	'''
+	# sort the common artists based on the cumulative score
+	top_artists = sorted(common_artists.iteritems(), reverse=True)
+	
+	print 'Max score:', max
+	print 'Point score:', score
+	print 'Score:', (100 * score) / max
+	print top_artists
 	return data
 
 # Makes a last.fm api call for the given user, returning their top 50 artists for the past month
@@ -52,6 +60,6 @@ def get_artist_dictionary(data):
 	for artist in data['topartists']['artist']:
 		rank = artist['@attr']['rank']
 		name = artist['name']
-		print rank, name
+		#print rank, name
 		result[name] = rank
 	return result
