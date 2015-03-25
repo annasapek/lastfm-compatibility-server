2692728ff46d7ca2aba361dff391b5ad9deec8c8
Anna Sapek <asapek@gmail.com>
2015-03-25 00:12:06 -0500

    Adds image URLS to JSON top artists data

--- calculate.py
+++ calculate.py
@@ -1,25 +1,35 @@
 from requests import get
-import json
+import json, operator
 from config import LASTFM_API_KEY
 
 def get_score(me, friend):
 	period = '1month'
-	data = [api_call_artists(me, period), api_call_artists(friend, period)]
-	
-	# check for valid usernames
-	for result in data:
-		if 'error' in result:
-			return {'status': 1}
+	data = {me: api_call_artists(me, period), friend: api_call_artists(friend, period)}
 	
 	# JSON result
-	result = {'status': 0, 'result': {'user_1': me, 'user_2': friend}}
+	result = {'status' : 0}
+	
+	# check for errors
+	for name, response in data.iteritems():
+		if 'error' in response:
+			result['status'] = 1
+			
+			# invalid usernames
+			if response['error'] == 6: 
+				if 'error_messages' in result:
+					result['error_messages'].append('%s is not a valid Last.fm username.' % name)
+				else:
+					result['error_messages'] = ['%s is not a valid Last.fm username.' % name]
+	
+	# return if there were errors
+	if result['status'] != 0: 
+		return result
+	
+	result['result'] = {'user_1': me, 'user_2': friend}
 	
 	# dictionaries from artist to rank
-	my_artists = get_artist_dictionary(data[0])
-	friend_artists = get_artist_dictionary(data[1])
-	
-	# score
-	score = 0
+	my_artists = get_artist_dictionary(data[me])
+	friend_artists = get_artist_dictionary(data[friend])
 	
 	# min_length = n + 1, so (min_length - rank) ranges from 1 to n
 	min_length = min(len(my_artists), len(friend_artists)) + 1	
@@ -27,28 +37,29 @@ def get_score(me, friend):
 	# (n + 1)(n) 
 	max = min_length * (min_length - 1)
 	
-	# common artists
+	# common artists, a dictionary, {'score': {'name', 'image'}}
 	common_artists = {}
+	
+	# topartists, a list of dictionaries, [{'name', 'image'}]
 	result['result']['topartists'] = []
 	
 	# compute a cumulative score for each shared artist
-	for artist, rank in my_artists.iteritems():
+	for artist, data in my_artists.iteritems():
+		# check if the other user also listens to this artist
 		if artist in friend_artists.iterkeys():
-			my_score = min_length - int(rank)
-			friend_score = min_length - int(friend_artists[artist])
+			my_score = min_length - int(data['rank'])
+			friend_score = min_length - int(friend_artists[artist]['rank'])
 			sum_score = my_score + friend_score
-			common_artists[artist] = sum_score
+			common_artists[sum_score] = {'name': artist, 'image': data['image']}
 			score += sum_score
-			print rank, artist, friend_artists[artist], my_score, friend_score, my_score + friend_score
+	
+	# cumulative score
+	score = sum(x for x in common_artists.iterkeys())
 	
 	# sort the common artists based on the cumulative score
-	result['result']['topartists'] = sorted(common_artists.items(), key=lambda (k, v): common_artists[k], reverse=True)
+	result['result']['topartists'] = [v for (k, v) in sorted(common_artists.items(), reverse=True)]
 	result['result']['score'] = (100 * score) / max
 	
-	print 'Max score:', max
-	print 'Point score:', score
-	print 'Score:', result['result']['score']
-	print result['result']['topartists']
 	return result
 
 # Makes a last.fm api call for the given user, returning their top 50 artists for the past month
@@ -61,12 +72,12 @@ def api_call_artists(username, period):
 	
 	return json_data
 
-# Returns a dictionary from artist to rank from the given JSON data
+# Returns a dictionary from artist to {rank, image} from the given JSON data
 def get_artist_dictionary(data):
 	result = {}
 	for artist in data['topartists']['artist']:
 		rank = artist['@attr']['rank']
 		name = artist['name']
-		#print rank, name
-		result[name] = rank
+		image = artist['image'][1]['#text']
+		result[name] = {'rank': rank, 'image': image}
 	return result
