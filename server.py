05b13f9050076b744cebb6bc91b62401ef0318e4
Christopher Su <christophersu9@gmail.com>
2015-04-03 20:22:04 -0700

    comments

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 import calculate
 
-# server info 
+# serve front end 
 @app.route('/')
 def index():
     return app.send_static_file('app.html')
